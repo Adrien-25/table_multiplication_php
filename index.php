a5fea67f5f25f7aa8a7c5d37d990092cbd11913c
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-06-08 11:35:58 +0200

    Add - Ajout page index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Acceuil</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+      <?php include("header.php"); ?>
+    </div>
+  </body>
+
+</html>
