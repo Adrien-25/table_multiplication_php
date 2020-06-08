026914d6b3cadd83656a1d09b2369659786d5fd7
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-06-08 11:39:53 +0200

    Add - Ajout de la page header
    Ajout du code php du header qui sera ajout sur toutes les autres pages

---
+++ header.php
@@ -0,0 +1,16 @@
+<header >
+  <div class="container nav_layout">
+    <nav id="menu">        
+        <ul>
+            
+            <li><a href="page1.php">Table de 3</a></li> 
+            <li><a href="page2.php">Choix d'une table</a></li> 
+            <li><a href="page3.php">Affichage d'une table</a></li>
+            <li><a href="page4.php">Révision</a></li>
+            <li><a href="page5.php">Super révision</a></li>
+            <li><a href="page6.php">Révision AJAX</a></li>
+        </ul> 
+    </nav>
+  </div>
+</header>
+
